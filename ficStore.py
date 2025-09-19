dd26a602989f6b21c252dd58697da2d872ca8690
Rafael Gandis de Brito <111640779+rafaelgandis@users.noreply.github.com>
2025-09-20 05:00:46 +0900

    Adicionar projeto sem arquivos sensíveis

--- ficStore.py
+++ ficStore.py
@@ -1,3 +1,27 @@
-ola = 'Olá, Mundo!'
+df = pd.read_csv('vendas_relatorio_300MB.csv')
 
-print(ola)
+caminho_csv = "vendas_relatorio_300MB.csv"
+
+df = pd.read_csv(caminho_csv)
+
+# Conexão com PostgreSQL
+usuario = "postgres"
+senha = 
+host = "localhost"
+porta = "5432"
+banco = "LojaFicticia"
+
+engine = create_engine(f"postgresql+psycopg2://{usuario}:{senha}@{host}:{porta}/{banco}")
+
+df = df.drop_duplicates()
+
+# Enviar para PostgreSQL e criar tabela automaticamente
+df.to_sql("vendas", engine, if_exists="replace", index=False)
+
+# Remove duplicados no DataFrame antes de enviar
+
+
+
+
+print("✅ CSV importado para PostgreSQL com sucesso!")
+
