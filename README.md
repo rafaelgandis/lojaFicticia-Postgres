c3800c8422485ff440f7dbf81572579a91591f76
Rafael Gandis de Brito <111640779+rafaelgandis@users.noreply.github.com>
2025-09-24 01:58:10 +0900

    escondendo dados sensiveis

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lojaFicticia-Postgres
 
 Neste arquivo eu usei um código de Pythion para abrir um arquivo CSV e envia-lo para o Postgres localmente.
+
+Tamanho de arquivo que eram de 300MB, o Github reduziu para caber na plataforma.
--- lojaFicticia.py
+++ lojaFicticia.py
@@ -3,33 +3,6 @@ import matplotlib.pyplot as plt
 import psycopg2
 from sqlalchemy import create_engine
 
-# Conexão com PostgreSQL
-# try:
-#     conn = psycopg2.connect(
-#         host="localhost",
-#         database="LojaFicticia",
-#         user="postgres",
-#         password="Rafagb1980!"
-#     )
-
-
-#     cur = conn.cursor()
-#     cur.execute("SELECT version();")
-#     versao = cur.fetchone()
-#     print("✅ Conexão bem-sucedida!")
-#     print("Versão do PostgreSQL:", versao)
-
-# except Exception as e:
-#         print("❌ Erro ao conectar:", e)
-
-# finally:
-#         if 'cur' in locals():
-#             cur.close()
-#         if 'conn' in locals():
-#             conn.close()
-
-
-
 df = pd.read_csv('vendas_relatorio_300MB.csv')
 
 caminho_csv = "vendas_relatorio_300MB.csv"
@@ -38,7 +11,7 @@ df = pd.read_csv(caminho_csv)
 
 # Conexão com PostgreSQL
 usuario = "postgres"
-senha = "Rafagb1980!"
+#senha = "     "
 host = "localhost"
 porta = "5432"
 banco = "LojaFicticia"
